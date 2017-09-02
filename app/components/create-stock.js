91e36334bf3382bc8fb070608c36c357495fc6bd
WillWhitmey <will.whitmey@gojip2p.com>
2017-09-02 18:56:25 +0100

    Alpha 1.1.1

--- app/components/create-stock.js
+++ app/components/create-stock.js
@@ -7,7 +7,7 @@ export default Ember.Component.extend({
 
   actions: {
     createStock() {
-      let data = this.get('store').createRecord('stock-summary', {
+      let data = this.get('store').createRecord('stock', {
         name: this.get('name'),
         price: this.get('price'),
         image: this.get('image')
